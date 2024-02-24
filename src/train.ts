c3bf72dc13bd69c54c1f97cda703261950656d57
ulushbey <ulushbey.h@gmail.com>
2024-02-25 00:49:37 +0900

    feat: integration of environmental variables & TASK H

---
+++ src/train.ts
@@ -0,0 +1,8 @@
+//MIT Task H
+
+function getReverse(str: string): string {
+    return str.split('').reverse().join('');
+}
+
+console.log(getReverse("hello")); 
+console.log(getReverse("jacob"));  
--- train.js
+++
@@ -1,9 +0,0 @@
-//MIT Task G
-
-function getHighestIndex(arr) {
-    const sortedArr = arr.slice().sort((a, b) => b - a);
-    return arr.indexOf(sortedArr[0]);
-  }
-  
-  console.log(getHighestIndex([5, 21, 12, 21, 8]));
-  
