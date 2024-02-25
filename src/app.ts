27254c4db721c4ef1ba9e8a08a189e13d795a28e
ulushbey <ulushbey.h@gmail.com>
2024-02-25 15:46:54 +0900

    feat: express integration

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import path from 'path';
+
+
+/** 1 - Entrance **/
+const app = express();
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+/** 2 - Session **/
+
+/** 3 - Views **/
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/** 4 - Routers **/
+
+
+export default app;
