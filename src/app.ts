48df797e732356c948004623963641f4c12d3ac7
ulushbey <ulushbey.h@gmail.com>
2024-02-27 08:26:24 +0900

    fix: modify routing system and controllers

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import path from 'path';
 import router from "./router";
+import routerAdmin from "./routerAdmin";
 
 /** 1 - Entrance **/
 const app = express();
@@ -16,7 +17,8 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 /** 4 - Routers **/
-app.use("/", router);  
+app.use("/admin", routerAdmin); // EJS 
+app.use("/", router);           // REACt
 
 
 export default app;
