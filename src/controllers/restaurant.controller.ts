6b8cee289159fda38ff0e89a938704fdd175f941
ulushbey <ulushbey.h@gmail.com>
2024-02-29 18:16:44 +0900

    feat: create project standards

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -7,6 +7,7 @@ import MemberService from "../models/Member.service";
 const restaurantController: T = {};
 restaurantController.goHome  = (req: Request, res: Response) => {
     try {
+        console.log('goHome');
         res.send('Home Page');
     } catch (err) {
         console.log("Error, goHome:", err);
@@ -15,6 +16,7 @@ restaurantController.goHome  = (req: Request, res: Response) => {
 
 restaurantController.getLogin  = (req: Request, res: Response) => {
     try {
+        console.log('getLogin');
         res.send('Login Page');
     } catch (err) {
         console.log("Error, getLogin:", err);
@@ -23,6 +25,7 @@ restaurantController.getLogin  = (req: Request, res: Response) => {
 
 restaurantController.getSignup  = (req: Request, res: Response) => {
     try {
+        console.log('getSignup');
         res.send('Signup Page');
     } catch (err) {
         console.log("Error, getSignup:", err);
--- src/train.ts
+++ src/train.ts
@@ -1,11 +1,1 @@
-// MIT TASK H
-
-function getPositive(arr: number[]) : string {
-    const a: number[] = arr.filter(num => num > 0);  
-    return a.join('');
-  }
-  
-  
-  console.log(getPositive([1, -4, 2, ])); 
-  
 
