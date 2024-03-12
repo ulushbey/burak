f51706b095df99222e7fc50f9e1da2381b4f3e72
ulushbey <ulushbey.h@gmail.com>
2024-03-13 01:03:07 +0900

    feat: configure product Schema model

--- src/models/Poduct.service.ts
+++ src/models/Poduct.service.ts
@@ -1,7 +1,13 @@
+import ProductModel from "../schema/Product.model";
 
 
 class ProductService {
+    private readonly productModel;
 
+    constructor() {
+        this.productModel = ProductModel;
+
+    }
 }
 
 export default ProductService;
