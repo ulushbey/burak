47a11fd991568e21d53801dca3a9f9409a5fa694
ulushbey <ulushbey.h@gmail.com>
2024-03-15 03:53:48 +0900

    fix: modify updateChosenProduct business logic

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -48,6 +48,10 @@ productController.createNewProduct  = async (req: AdminRequest, res: Response) =
 productController.updateChosenProduct  = async (req: Request, res: Response) => {
     try {
         console.log('updateChosenProduct');
+        const id = req.params.id;
+        const result = await productService.updateChosenProduct(id, req.body);
+        res.status(HttpCode.OK).json({data: result});
+
     }   catch (err) {
         console.log("Error, updateChosenProduct:", err);
         if(err instanceof Errors) res.status(err.code).json(err);
--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -27,3 +27,18 @@ export interface  ProductInput {
     productImages?: string[];
     productViews?: number;
 }
+
+
+export interface  ProductUpdateInput {
+    _id: ObjectId;
+    productStatus?: ProductStatus;
+    productCollection?: ProductCollection;
+    productName?: string;
+    productPrice?: number;
+    productLeftCount?: number;
+    productSize?: ProductSize;
+    productVolume?: number;
+    productDesc?: string;
+    productImages?: string[];
+    productViews?: number;
+}
