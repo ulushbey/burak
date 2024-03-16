329a1bc7994cd0a50da36c9aa903dd1626771251
ulushbey <ulushbey.h@gmail.com>
2024-03-16 18:15:41 +0900

    feat: develop updateChosenUser api

--- src/libs/types/member.ts
+++ src/libs/types/member.ts
@@ -35,6 +35,18 @@ export interface LoginInput {
     memberPassword: string;
 }
 
+export interface MemberUpdateInput {
+    _id: ObjectId;
+    memberStatus?: MemberStatus;
+    memberNick?: string;
+    memberPhone?: string;
+    memberPassword?: string;
+    memberAdress?: string;
+    memberDesc?: string;
+    memberImage?: string;
+    
+}
+
 export interface AdminRequest extends Request {
     member: Member;
     session: Session & {member: Member};
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -41,6 +41,7 @@ routerAdmin.post(
     productController.updateChosenProduct);
 
 /** User */
-routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers)
+routerAdmin.get("/user/all", restaurantController.verifyRestaurant, restaurantController.getUsers);
+routerAdmin.post("/user/edit", restaurantController.verifyRestaurant, restaurantController.updateChosenUser);
 
 export default routerAdmin;
