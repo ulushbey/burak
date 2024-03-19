6dec0a92c241ef97e0bfa50c61ad528bc24e3bda
ulushbey <ulushbey.h@gmail.com>
2024-03-20 01:37:35 +0900

    fix: correct login process logic

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -62,7 +62,7 @@ restaurantController.processSignup  = async (req: AdminRequest, res: Response) =
     }   catch (err) {
         console.log("Error, processSignup:", err);
         const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-        res.send(`<script> alert ("${message}"); window.location.replace('admin/signup') </script>`);
+        res.send(`<script> alert ("${message}"); window.location.replace('/admin/signup') </script>`);
     } 
 }
 
@@ -81,7 +81,7 @@ restaurantController.processLogin  = async (req: AdminRequest, res: Response) =>
     } catch (err) {
         console.log("Error, processLogin:", err);
         const message = err instanceof Errors ? err.message : Message.SOMETHING_WENT_WRONG;
-        res.send(`<script> alert ("${message}"); window.location.replace('admin/login') </script>`);
+        res.send(`<script> alert ("${message}"); window.location.replace('/admin/login') </script>`);
     } 
 }
 
